bcfc3b36f42c97a54534004acafd4a26c79d78d9
KrasodomskaAnna <94318341+KrasodomskaAnna@users.noreply.github.com>
2023-05-04 01:07:21 +0200

    code

---
+++ node.py
@@ -0,0 +1,114 @@
+import copy
+
+import numpy as np
+
+
+class Node:
+    def __init__(self):
+        self.left_child = None
+        self.right_child = None
+        self.feature_idx = None
+        self.feature_value = None
+        self.node_prediction = None
+
+    def gini_best_score(self, y, possible_splits):
+        best_gain = -np.inf
+        best_idx = 0
+
+        # TODO find position of best data split
+        for possible_split in possible_splits:
+            y_left = y[:possible_split + 1]
+            y_right = y[possible_split + 1:]
+
+            left_positive = np.count_nonzero(y_left)
+            left_negative = len(y_left) - left_positive
+            right_positive = np.count_nonzero(y_right)
+            right_negative = len(y_right) - right_positive
+
+            left = left_positive + left_negative
+            right = right_positive + right_negative
+
+            gini_left = 1 - (left_positive / left) ** 2 - (left_negative / left) ** 2
+            gini_right = 1 - (right_positive / right) ** 2 - (right_negative / right) ** 2
+
+            gini_gain = 1 - (left / (left + right)) * gini_left - (right / (left + right)) * gini_right
+
+            if gini_gain > best_gain:
+                best_gain = gini_gain
+                best_idx = possible_split
+
+        return best_idx, best_gain
+
+    def split_data(self, X, y, idx, val):
+        left_mask = X[:, idx] < val
+        return (X[left_mask], y[left_mask]), (X[~left_mask], y[~left_mask])
+
+    def find_possible_splits(self, data):
+        possible_split_points = []
+        for idx in range(data.shape[0] - 1):
+            if data[idx] != data[idx + 1]:
+                possible_split_points.append(idx)
+        return possible_split_points
+
+    def find_best_split(self, X, y, feature_subset):
+        best_gain = -np.inf
+        best_split = None
+
+        # TODO implement feature selection
+        n_features = X.shape[1]
+        if feature_subset:
+            feature_subset = np.random.choice(n_features, feature_subset, replace=False)
+        else:
+            feature_subset = range(X.shape[1])
+
+        for d in feature_subset:
+            order = np.argsort(X[:, d])
+            y_sorted = y[order]
+            possible_splits = self.find_possible_splits(X[order, d])
+            idx, value = self.gini_best_score(y_sorted, possible_splits)
+            if value > best_gain:
+                best_gain = value
+                best_split = (d, [idx, idx + 1])
+
+        if best_split is None:
+            return None, None
+
+        best_value = np.mean(X[best_split[1], best_split[0]])
+
+        return best_split[0], best_value
+
+    def predict(self, x):
+        if self.feature_idx is None:
+            return self.node_prediction
+        if x[self.feature_idx] < self.feature_value:
+            return self.left_child.predict(x)
+        else:
+            return self.right_child.predict(x)
+
+    def train(self, X, y, params):
+
+        self.node_prediction = np.mean(y)
+        if X.shape[0] == 1 or self.node_prediction == 0 or self.node_prediction == 1:
+            return True
+
+        self.feature_idx, self.feature_value = self.find_best_split(X, y, params["feature_subset"])
+        if self.feature_idx is None:
+            return True
+
+        (X_left, y_left), (X_right, y_right) = self.split_data(X, y, self.feature_idx, self.feature_value)
+
+        if X_left.shape[0] == 0 or X_right.shape[0] == 0:
+            self.feature_idx = None
+            return True
+
+        # max tree depth
+        if params["depth"] is not None:
+            params["depth"] -= 1
+        if params["depth"] == 0:
+            self.feature_idx = None
+            return True
+
+        # create new nodes
+        self.left_child, self.right_child = Node(), Node()
+        self.left_child.train(X_left, y_left, copy.deepcopy(params))
+        self.right_child.train(X_right, y_right, copy.deepcopy(params))
